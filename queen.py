41c4f4a3fc285d2db83d2bff3168ea75b35fccfc
juan cruz <j.sacchi@alumno.um.edu.ar>
2024-08-24 21:57:58 -0300

    sexta entrega

--- queen.py
+++ queen.py
@@ -10,3 +10,19 @@ class Board:
             self.positions.append(col)
         self.positions[0][3] = Queen("Black") # Black
         self.positions[7][3] = Queen("White") # White
+
+    # forma del tablero
+
+    def __str__(self):
+        board_str = ""
+        for row in self.__positions__:
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
+                else:
+                    board_str += " "
+            board_str += "\n"
+        return board_str
+        
+    def get_piece(self, row, col):
+        return self.__positions__[row][col]

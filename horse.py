9b44035088afd0621e9f5082e8d1636cc3761869
juan cruz <j.sacchi@alumno.um.edu.ar>
2024-08-25 21:30:14 -0300

    septima entrega

--- horse.py
+++ horse.py
@@ -12,3 +12,19 @@ class Board:
         self.positions[0][6] = Horse("Black") # Black
         self.positions[7][1] = Horse("White") # White
         self.positions[7][6] = Horse("White") # White
+
+    # forma del tablero
+
+    def __str__(self):
+        board_str = ""
+        for row in self.__positions__:
+            for cell in row:
+                if cell is not None:
+                    board_str += str(cell)
+                else:
+                    board_str += " "
+            board_str += "\n"
+        return board_str
+        
+    def get_piece(self, row, col):
+        return self.__positions__[row][col]

afb72f06e80a630fcf8b63d45690b9b05378a5da
juan cruz <j.sacchi@alumno.um.edu.ar>
2024-09-02 23:16:02 -0300

    quinta entrega

--- bishop.py
+++ bishop.py
@@ -1,4 +1,6 @@
-from pieces import Bishop
+# posicion del ALFIL
+
+from pieces import Bishop, Piece
 
 class Board:
     def __init__(self):
@@ -13,7 +15,7 @@ class Board:
         self.positions[7][2] = Bishop("White") # White
         self.positions[7][5] = Bishop("White") # White
 
-    # forma del tablero
+# forma del tablero
 
     def __str__(self):
         board_str = ""
@@ -28,12 +30,25 @@ class Board:
         
     def get_piece(self, row, col):
         return self.__positions__[row][col]
-    
-    # movimiento del bishop
 
-class pieces:
-    def __init__(self):
-        self.positions[0][2] = Bishop("Black") # Black
-        self.positions[0][5] = Bishop("Black") # Black
-        self.positions[7][2] = Bishop("White") # White
-        self.positions[7][5] = Bishop("White") # White
+# Movimiento del ALFIL
+
+class Bishop(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = 'B' if color == 'WHITE' else 'b'
+
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        # El alfil se mueve en diagonal, por lo que la diferencia entre filas y columnas debe ser igual
+        if abs(to_row - from_row) == abs(to_col - from_col):
+            # Verificar que no haya piezas en el camino
+            step_row = 1 if to_row > from_row else -1
+            step_col = 1 if to_col > from_col else -1
+            for i in range(1, abs(to_row - from_row)):
+                if board[from_row + i * step_row][from_col + i * step_col] is not None:
+                    return False
+            # El movimiento es válido si la casilla de destino está vacía o contiene una pieza del oponente
+            destination_piece = board[to_row][to_col]
+            if destination_piece is None or destination_piece.color != self.color:
+                return True
+        return False

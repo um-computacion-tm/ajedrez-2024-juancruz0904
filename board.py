635bb3ec4edac616d319bde972236544322871b3
juan cruz <j.sacchi@alumno.um.edu.ar>
2024-09-19 21:26:42 -0300

    quinta entrega

--- board.py
+++ board.py
@@ -1,28 +1,50 @@
-from rook import Rook
-
 class Board:
     def __init__(self):
-        self.__positions__ = []
-        for _ in range(8):
-            col = []
-            for _ in range(8):
-                col.append(None)
-            self.__positions__.append(col)
-        self.__positions__[0][0] = Rook("BLACK") # Black
-        self.__positions__[0][7] = Rook("BLACK") # Black
-        self.__positions__[7][7] = Rook("WHITE") # White
-        self.__positions__[7][0] = Rook("WHITE") # White
+        # Inicializamos un tablero de 8x8 con None en todas las posiciones
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+        self.setup_board()
+
+# Posiciones de las piesas
+
+    def setup_board(self):
+        # Colocamos los peones en su posición inicial
+        for i in range(8):
+            self.board[1][i] = Pawn('BLACK')
+            self.board[6][i] = Pawn('WHITE')
+
+        # Colocamos las torres en su posición inicial
+        self.board[0][0] = self.board[0][7] = Rook('BLACK')
+        self.board[7][0] = self.board[7][7] = Rook('WHITE')
+
+        # Colocamos los caballos en su posición inicial
+        self.board[0][1] = self.board[0][6] = Horse('BLACK')
+        self.board[7][1] = self.board[7][6] = Horse('WHITE')
+
+        # Colocamos los alfiles en su posición inicial
+        self.board[0][2] = self.board[0][5] = Bishop('BLACK')
+        self.board[7][2] = self.board[7][5] = Bishop('WHITE')
+
+        # Colocamos las reinas en su posición inicial
+        self.board[0][3] = Queen('BLACK')
+        self.board[7][3] = Queen('WHITE')
+
+        # Colocamos los reyes en su posición inicial
+        self.board[0][4] = King('BLACK')
+        self.board[7][4] = King('WHITE')
+
+# Construye el tablero
 
     def __str__(self):
         board_str = ""
-        for row in self.__positions__:
-            for cell in row:
-                if cell is not None:
-                    board_str += str(cell)
+        for row in self.board:
+            for piece in row:
+                if piece is None:
+                    board_str += ". "
                 else:
-                    board_str += " "
+                    board_str += str(piece) + " "
             board_str += "\n"
-        return board_str
-
+        return board_str   
+        
     def get_piece(self, row, col):
-        return self.__positions__[row][col]
+        # Devuelve la pieza en la posición dada
+        return self.board[row][col]
